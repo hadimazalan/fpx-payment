def31d3ed781a2d13659a182f54b93e3498a25c4
HM <hadimazalan@gmail.com>
2023-04-09 00:50:28 +0800

    update bank flow update create

--- database/migrations/2021_01_10_100000_create_banks_table.php
+++ database/migrations/2021_01_10_100000_create_banks_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use JagdishJP\FpxPayment\Models\Bank;
+use Hadimazalan\FpxPayment\Models\Bank;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
